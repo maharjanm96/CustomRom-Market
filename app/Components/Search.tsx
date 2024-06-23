e71795d3ec02182c20641eed4b57bbb924957edc
Manish Maharjan <maharjanm96@gmail.com>
2024-06-23 09:52:38 +0545

    Home UI

--- app/Components/Search.tsx
+++ app/Components/Search.tsx
@@ -5,7 +5,7 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const Search = () => {
   return (
-    <div className="bg-white p-16 flex justify-center items-center ">
+    <div className="bg-white p-12 flex justify-center items-center ">
       <div className="w-full max-w-2xl">
         <button className="w-full p-2 text-lg border-2 rounded-lg focus:outline-none bg-white flex items-center hover:bg-gray-100 cursor-text ">
           <SearchIcon className="mr-2 " />
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,41 @@
+"use client";
+
 import React from "react";
+import { TypewriterEffectSmooth } from "./Components/ui/Typewriter-effect";
 
 export default function Home() {
-  return <div>Home</div>;
+  const words = [
+    {
+      text: "Unlock The",
+    },
+    {
+      text: "Full Potential",
+    },
+    {
+      text: "Of Your Device With",
+    },
+    // {
+    //   text: "with",
+    // },
+    {
+      text: "Custom ROMs.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[25rem]  ">
+      <p className="text-black text-xl ">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="w-40 h-10 rounded-xl bg-white border-2 text-black border-black text-sm">
+          Login
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-black text-white border border-black  text-sm">
+          Signup
+        </button>
+      </div>
+    </div>
+  );
 }
