076e09122ad8ab898147ed1e11faf3852a495dc7
Manish Maharjan <maharjanm96@gmail.com>
2024-07-04 13:44:18 +0545

    UserSchema Modified

--- app/models/UserSchema.ts
+++ app/models/UserSchema.ts
@@ -1,11 +1,24 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IUser extends Document {
+  name: string;
+  contact: number;
   email: string;
   password: string;
 }
 
 const UserSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    unique: false,
+  },
+
+  contact: {
+    type: Number,
+    required: [true, "Contact is required"],
+    unique: true,
+  },
   email: {
     type: String,
     required: [true, "Email is required"],
--- app/pages/api/auth/signup/route.ts
+++ app/pages/api/auth/signup/route.ts
@@ -6,35 +6,47 @@ import bcrypt from "bcrypt";
 export async function POST(req: NextRequest) {
   await dbConnect();
 
-  const { email, password } = await req.json();
-
-  const existingUser = await UserModel.findOne({ email });
-  if (existingUser) {
-    console.log("User already exists");
-    return NextResponse.json(
-      { message: "User already exists" },
-      { status: 400 }
-    );
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const newUser = new UserModel({
-    email,
-    password: hashedPassword,
-  });
-
+  const { name, contact, email, password } = await req.json();
   try {
+    const existingEmail = await UserModel.findOne({ email });
+    if (existingEmail) {
+      console.log("User with email already exists");
+      return NextResponse.json(
+        { message: "User with email already exists" },
+        { status: 400 }
+      );
+    }
+
+    const existingContact = await UserModel.findOne({ contact });
+    if (existingContact) {
+      console.log("Contact already exists");
+      return NextResponse.json(
+        {
+          message: "Contact already exists",
+        },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new UserModel({
+      name,
+      contact,
+      email,
+      password: hashedPassword,
+    });
+
     await newUser.save();
     console.log("User created successfully");
     return NextResponse.json(
-      { message: "User created successfully" },
+      { message: "User created successfully", data: newUser },
       { status: 201 }
     );
   } catch (error) {
     return NextResponse.json(
-      { message: "Error creating user" },
+      { message: "Error creating user", error },
       { status: 500 }
     );
   }
