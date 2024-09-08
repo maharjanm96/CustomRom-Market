c03672bd83aaf74726d545367b4e153316ff2bff
Manish Maharjan <maharjanm96@gmail.com>
2024-09-08 17:35:04 +0545

    LoginForm fix for deployment

--- app/auth/login/_components/LoginForm.tsx
+++ app/auth/login/_components/LoginForm.tsx
@@ -18,12 +18,11 @@ import { LoginSchema } from "@/schemas";
 import { login } from "@/actions/login";
 import { useSearchParams } from "next/navigation";
 import SocialButton from "@/components/Buttons/SocialButton";
+import { toast } from "sonner";
 
 export function LoginForm() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | undefined>("");
-
   const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(false);
   const callbackUrl = searchParams.get("callbackUrl");
 
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -34,32 +33,36 @@ export function LoginForm() {
     },
   });
 
-  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-    setLoading(true);
-    setError("");
-
+  async function onSubmit(values: z.infer<typeof LoginSchema>) {
     try {
-      const result = await login(values, callbackUrl);
-      console.log(result); // Add this line to check the response
-
-      if (result?.error) {
-        setError(result.error);
-        form.reset();
-      }
-    } catch (err) {
-      setError("Something went wrong. Please try again.");
+      setLoading(true);
+      const formData = {
+        email: values.email.toLowerCase(),
+        password: values.password,
+      };
+      await login(formData, callbackUrl).then((response) => {
+        if (!response) {
+          toast.success("Welcome to ROM Market");
+        }
+        if (response?.error) {
+          toast.error(response.error);
+        }
+        setLoading(false);
+      });
+    } catch (error: any) {
+      toast.error(`${error.message}`);
+      setLoading(false);
     } finally {
       setLoading(false);
     }
-  };
-
+  }
   return (
     <div className="flex justify-center mt-16">
       <div className="bg-white p-6 rounded-lg shadow-lg w-auto">
         <Form {...form}>
-          <FormLabel className="flex justify-start text-3xl text-black font-bold py-2">
+          <h2 className="flex justify-start text-3xl text-black font-bold py-2">
             Login
-          </FormLabel>
+          </h2>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
               control={form.control}
@@ -91,9 +94,13 @@ export function LoginForm() {
                 </FormItem>
               )}
             />
-            {error && <div className="text-red-500">{error}</div>}
-            <Button className="w-full" size="lg" disabled={loading}>
-              {loading ? "Logging in..." : "Login"}
+            <Button
+              disabled={loading}
+              variant={"default"}
+              type="submit"
+              className="w-full"
+            >
+              Login
             </Button>
 
             <span className="flex justify-center text-sm mt-4">
@@ -104,14 +111,14 @@ export function LoginForm() {
                 Sign Up!
               </span>
             </Link>
-            <FormLabel className="flex justify-center text-gray-500">
+            <div className="flex justify-center text-gray-500">
               or continue with
-            </FormLabel>
+            </div>
           </form>
         </Form>
-        <div className="mt-2"></div>
-        {/* You can add your social login buttons here */}
-        <SocialButton buttonText={"Google"} provider={"google"} />
+        <div className="mt-2 space-y-2">
+          <SocialButton buttonText={"Google"} provider={"google"} />
+        </div>
       </div>
     </div>
   );
